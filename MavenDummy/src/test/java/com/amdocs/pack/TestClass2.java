a34b94cfd5ab3cb51c875ebaded4e147f8c48de6
Nitish <Nitish@192.168.1.8>
2019-08-18 18:38:44 +0530

    VErsion 1.0.0 Fist commit

---
+++ MavenDummy/src/test/java/com/amdocs/pack/TestClass2.java
@@ -0,0 +1,47 @@
+package com.amdocs.pack;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+
+	@Test
+public void sum()
+{
+		System.out.println("This is the sum method");
+		int a =10;
+		int b =20;
+		int c =a+b;
+		Assert.assertEquals(c, 30);
+}
+
+	@Test
+	public void sub()
+	{	System.out.println("I am the sub");		
+		int a = 45;
+		int b =30;
+		int c = a-b;
+		Assert.assertEquals(c, 15);
+	}
+	
+	@Test
+	public void mult()
+	{	System.out.println("I am the multiplication");		
+		int a = 10;
+		int b =10;
+		int c = a*b;
+		Assert.assertEquals(c, 100);
+	}
+	
+	
+	@Test
+	public void div()
+	{	System.out.println("I am the division");		
+		int a = 45;
+		int b =5;
+		int c = a/b;
+		Assert.assertEquals(c, 9);
+	}
+	
+
+}
